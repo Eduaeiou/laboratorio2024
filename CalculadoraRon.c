e86da1735c2398109941b38c61a241dae42079a6
Eduaeiou <137584569+Eduaeiou@users.noreply.github.com>
2024-04-29 01:49:54 -0300

    Version 1

---
+++ CalculadoraRon.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,suma;
+    printf("Ingresar un numero: ");
+    scanf("%i",&num1);
+    printf("Ingresar el otro numero: ");
+    scanf("%i",&num2);
+    suma=num1+num2;
+    printf ("%i",suma);
+    return 0;
+}
