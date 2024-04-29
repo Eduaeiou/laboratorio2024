770f4067813a89b3c605f28f3de69107ac8c5bfc
Eduaeiou <137584569+Eduaeiou@users.noreply.github.com>
2024-04-29 01:58:50 -0300

    version 2

--- CalculadoraRon.c
+++ CalculadoraRon.c
@@ -2,12 +2,19 @@
 
 int main()
 {
-    int num1,num2,suma;
+    int num1,num2,suma,resta;
     printf("Ingresar un numero: ");
     scanf("%i",&num1);
     printf("Ingresar el otro numero: ");
     scanf("%i",&num2);
     suma=num1+num2;
-    printf ("%i",suma);
+    printf ("%i\n",suma);
+
+    printf("Ingresar un numero: ");
+    scanf("%i",&num1);
+    printf("Ingresar el otro numero: ");
+    scanf("%i",&num2);
+    resta=num1-num2;
+    printf ("%i\n",resta);
     return 0;
 }
