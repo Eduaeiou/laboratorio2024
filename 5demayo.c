fde31fb5352609e46e445ce4c86c0b4c9081ceca
AlexR1407 <143028612+AlexR1407@users.noreply.github.com>
2024-05-07 23:07:25 -0300

    Felicitaciones, ver error de un código
    El código tiene un error, puede arreglarlo y volver a subir así no pierde la practica.

--- 5demayo.c
+++ 5demayo.c
@@ -1,3 +1,4 @@
+//Felicitaciones por el logro de poder subir los archivos al github, espero que luego usted me enseñe las nuevas cosas que va aprendiendo.
 #include <stdio.h>
 #include <stdio.h>
 int main (){
