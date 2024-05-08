026f20e7aa3c449544b74f6c8c814ddf5bae7d57
AlexR1407 <143028612+AlexR1407@users.noreply.github.com>
2024-05-07 23:03:15 -0300

    Felicitaciones y debe corregir un error de código
    Trate de corregir el error de código y volver a subirlo así no pierde la practica :)

--- 5demayo.c
+++ 5demayo.c
@@ -1,3 +1,5 @@
+//Felicitaciones por su logra en aprender a usar github. Espero que las cositas nuevas que usted vaya aprendiendo me las comparta.
+//Le comento que este código tiene un error. Trate de descargar el archivo y modificarlo, luego lo vuelve a subir para tener la ultima version.
 #include <stdio.h>
 #include <stdio.h>
 int main (){
